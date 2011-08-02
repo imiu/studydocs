dab512aff3611c62a5e3dd884ed00b800d566bac
Alex Kudryashov <alex.kudryashov@gmail.com>
2011-08-02 17:24:53 +0200

    Finished with lphw

---
+++ books/lpyhw/gothonweb/bind/app.py
@@ -0,0 +1,19 @@
+import web
+
+urls = (
+    '/', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class Index:
+    def GET(self):
+        form = web.input(name="Nobody")
+        form = web.input(greet="Hello")
+        greeting = "%s, %s" % (form.greet, form.name)
+        return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+    app.run()
