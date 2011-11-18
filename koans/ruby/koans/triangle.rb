f11e5963d0263998ab65e030013c2f0d45744e04
Alex Kudryashov <alex.kudryashov@gmail.com>
2011-11-18 16:56:19 +0100

    Some more ruby koans

--- koans/ruby/koans/triangle.rb
+++ koans/ruby/koans/triangle.rb
@@ -14,7 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a <= 0 or b <= 0 or c <= 0:
+    raise TriangleError, "Zero lenght"
+  end
+  if a + b <= c or b + c <= a or c + a <= b:
+    raise TriangleError, "Some side is too small"
+  end
+  if a == b and b == c
+    :equilateral
+  elsif a == b or b == c or a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
