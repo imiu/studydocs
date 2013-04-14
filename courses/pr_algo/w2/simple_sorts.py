f370ceea9a73a68f52dd66b2e764765a0e003783
Alex Kudryashov <alex.kudryashov@gmail.com>
2013-04-14 15:37:49 +0200

    Fixing multiple errors in sort implementations =)

--- courses/pr_algo/w2/simple_sorts.py
+++ courses/pr_algo/w2/simple_sorts.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import random
 
 
 # moar pythonic approach
@@ -18,23 +19,49 @@ def selection_sort_pythonic(arr):
 
 # classic approach
 def selection_sort(arr):
+    a = arr[:]
     i = 0
-    while i < len(arr):
-        for j in range(i, len(arr)):
-            min_idx = i
-            if arr[j] < arr[min_idx]:
+    while i < len(a):
+        min_idx = i
+        for j in range(i, len(a)):
+            if a[j] < a[min_idx]:
                 min_idx = j
-            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        a[i], a[min_idx] = a[min_idx], a[i]
         i += 1
-    return arr
+    return a
 
 
 def insertion_sort(arr):
+    a = arr[:]
     i = j = 0
-    while i < len(arr):
+    while i < len(a):
         for j in range(i, 0, -1):
-            if arr[j] < arr[j - 1]:
-                arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            if a[j] < a[j - 1]:
+                a[j - 1], a[j] = a[j], a[j - 1]
             j += 1
         i += 1
-    return arr
+    return a
+
+
+def shell_sort(arr):
+    a = arr[:]
+    h = 1
+    n = len(a)
+    while h < n / 3:
+        h = 3 * h + 1
+    while h >= 1:
+        for i in range(h, n):
+            for j in range(i, h - 1, (- h)):
+                if a[j] < a[j - h]:
+                    a[j], a[j - h] = a[j - h], a[j]
+        h = h / 3
+        print a
+    return a
+
+
+def knuth_shuffle(arr):
+    a = arr[:]
+    for idx in range(0, len(a)):
+        r = random.randint(0, idx + 1)
+        a[idx], a[r] = a[r], a[idx]
+    return a
--- courses/pr_algo/w2/tests/test_simple_sorts.py
+++ courses/pr_algo/w2/tests/test_simple_sorts.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-from simple_sorts import selection_sort, insertion_sort
+from simple_sorts import (selection_sort, insertion_sort, shell_sort,
+                          knuth_shuffle)
+import random
 import pytest
 
 
 @pytest.fixture
 def ar_unsorted():
-    return [0, 9, 3, 5, 2, 3, 15, 27, 7, 33, 1, 13, 15]
+    return [32, 0, 9, 3, 5, 2, 3, 15, 27, 7, 33, 1, 13, 15]
 
 
 @pytest.fixture
 def ar_repetitive():
-    return [0, 9, 1, 5, 2, 3, 15, 15, 7, 33, 1, 13, 9]
+    return [32, 0, 9, 1, 5, 2, 3, 15, 15, 7, 33, 1, 13, 9]
 
 
 @pytest.fixture
@@ -19,15 +21,22 @@ def ar_sorted():
 
 
 def pytest_generate_tests(metafunc):
-    funcarglist = metafunc.cls.params
-    argnames = list(funcarglist[0])
-    metafunc.parametrize(argnames, [[funcargs[name] for name in argnames]
-                                    for funcargs in funcarglist])
+    if metafunc.cls.__name__ == 'TestSorts':
+        funcarglist = metafunc.cls.params
+        argnames = list(funcarglist[0])
+        metafunc.parametrize(argnames, [[funcargs[name] for name in argnames]
+                                        for funcargs in funcarglist])
 
 
 class TestSorts(object):
-    params = [dict(func=selection_sort),
-              dict(func=insertion_sort)]
+    params = [
+        dict(func=selection_sort),
+        dict(func=insertion_sort),
+        dict(func=shell_sort),
+    ]
+
+    def test_sorted(self, func, ar_sorted):
+        assert ar_sorted == func(ar_sorted)
 
     def test_unsorted(self, func, ar_unsorted):
         result = sorted(ar_unsorted)
@@ -37,5 +46,11 @@ class TestSorts(object):
         result = sorted(ar_repetitive)
         assert result == func(ar_repetitive)
 
-    def test_sorted(self, func, ar_sorted):
-        assert ar_sorted == func(ar_sorted)
+
+class TestShuffle(object):
+    def test_shuffle(self):
+        random.seed(2128506)
+        arr = [0, 1, 2, 3, 4, 5]
+        # with the seed
+        res = [5, 0, 4, 2, 3, 1]
+        assert res == knuth_shuffle(arr)
