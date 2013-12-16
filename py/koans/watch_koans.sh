9473b60ffae5c53eb819fbb92a4416d29fb0b5f4
Alex Kudryashov <alex.kudryashov@gmail.com>
2013-12-16 21:14:41 +0100

    Cleaning python directory

--- py/koans/watch_koans.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env zsh
-watchmedo shell-command --patterns='*' --recursive --command='./run.sh' ./koans
--- py/pytest/pytest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from __future__ import division
-import py.test
-
-
-def test_case():
-    """docstring for test_case"""
-    py.test.assert_almost_equal(first=0.3333, second=0.3333, prec=0.001)
